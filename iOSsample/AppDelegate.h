9da557622ee250508440065da4b432f8b5a78ced
minato <ateliee@gmail.com>
2014-06-22 23:36:32 +0900

    Initial Commit

---
+++ iOSsample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOSsample
+//
+//  Created by ateliee on 2014/06/22.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOSsample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iOSsample
+//
+//  Created by ateliee on 2014/06/22.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
