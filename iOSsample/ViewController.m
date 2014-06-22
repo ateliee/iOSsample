561cf4e9fe668617fb385a2a8ba3d0ee1de2f18f
minato <ateliee@gmail.com>
2014-06-22 23:50:44 +0900

    test add

--- iOSsample/ViewController.m
+++ iOSsample/ViewController.m
@@ -18,6 +18,14 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    // ここから記述
+    UITextView *textView = [[UITextView alloc] initWithFrame:CGRectMake(0, 0, 320, 460)];
+    textView.text = @"Hello World !!";
+    textView.textAlignment = UITextAlignmentCenter;
+    textView.font = [UIFont fontWithName:@"Arial" size:24.0f];
+    textView.backgroundColor = [UIColor colorWithRed:1.0 green:0.0 blue:0.0 alpha:1.0];
+    [self.view addSubview:textView];
 }
 
 - (void)didReceiveMemoryWarning
